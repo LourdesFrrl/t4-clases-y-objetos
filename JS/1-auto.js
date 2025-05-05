3a5f60b34d9367147173d3a3be6118a6bfffa11a
LourdesFrrl <ferraiolimarialourdesfacultad@gmail.com>
2025-05-05 17:59:06 -0300

    agrege el prier ejercicio

---
+++ JS/1-auto.js
@@ -0,0 +1,29 @@
+const auto = {
+    color: "Rojo",
+    marca: "Toyota",
+    modelo: "Corolla",
+    encendido: false,
+  
+    encender: function() {
+      if (!this.encendido) {
+        this.encendido = true;
+        console.log("Auto encendido");
+      } else {
+        console.log("El auto ya está encendido");
+      }
+    },
+  
+    apagar: function() {
+      if (this.encendido) {
+        this.encendido = false;
+        console.log("El auto se apagó");
+      } else {
+        console.log("El auto ya está apagado");
+      }
+    }
+  };
+  
+
+  auto.encender();
+  auto.apagar();  
+  
