8d577e4e9d10224b08ad8852cb4c05d71787d42e
LourdesFrrl <ferraiolimarialourdesfacultad@gmail.com>
2025-05-05 19:29:26 -0300

    agrege un boceto del ejercicio 5

---
+++ JS/5-generaciones.js
@@ -0,0 +1,46 @@
+/*
+5- Crea una clase llamada Persona que siga las siguientes condiciones:
+Sus propiedades son: nombre, edad, DNI, sexo (H hombre, M mujer), peso y altura, año de nacimiento. Si quieres añadir alguna propiedad extra puedes hacerlo.
+Los métodos que se debe poder utilizar  son:
+mostrarGeneracion: este método debe mostrar un mensaje indicando a qué generación pertenece la persona creada y cual es el rasgo característico de esta generación.
+Para realizar este método tener en cuenta la siguiente tabla de generaciones:
+
+esMayorDeEdad: indica si es mayor de edad, devuelve un mensaje indicando que la persona es mayor de edad.
+mostrarDatos: devuelve toda la información del objeto.
+generaDNI(): genera un número aleatorio de 8 cifras.
+
+
+*/
+
+/*
+class persona {
+constructur (nombre edad, dni, sexo){
+this.nombre= nombre
+this.nombre= jdjdjd
+this.nombre= kkk
+
+mostrarGeneracion(){
+if(thisedad<=55){
+documentawriteline mostrando la generacion }if else{
+}if else
+
+}
+}
+mayorDeEdad(){
+if(this.edad>m16){
+docuemnte.write("e mayor de edad")}
+}else{
+documente("es mayor de edad")}
+
+mostrardatos(){
+usar un return
+}
+
+generadDNI(){
+for(let i=0; i<=8, i++){
+math.randon.mathfloor (o algo asi)
+}
+}
+
+}
+*/
