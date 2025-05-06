dc9e43e3079034f74f2ae1f7764e3d94cbc6b989
LourdesFrrl <ferraiolimarialourdesfacultad@gmail.com>
2025-05-06 20:03:55 -0300

    agrege un boceto de como hacer el ejercico 7

---
+++ JS/7-agenda.js
@@ -0,0 +1,72 @@
+/*
+7- Nos piden realizar una agenda telefónica de contactos.
+
+Un contacto está definido por un nombre y un teléfono. Se considera que un contacto es igual a otro cuando sus nombres son iguales.
+
+Una agenda de contactos está formada por un conjunto de contactos. Se podrá crear de dos formas, indicando nosotros el tamaño o con un tamaño por defecto (10).
+
+Los métodos de la agenda serán los siguientes:
+
+aniadirContacto(Contacto): Añade un contacto a la agenda, sino la agenda no puede almacenar más contactos indicar por pantalla.
+existeContacto(Contacto): indica si el contacto pasado existe o no.
+listarContactos(): Lista toda la agenda
+buscarContacto(nombre): busca un contacto por su nombre y muestra su teléfono.
+eliminarContacto(Contacto c): elimina el contacto de la agenda, indica si se ha eliminado o no por pantalla
+agendaLlena(): indica si la agenda está llena.
+huecosLibres(): indica cuántos contactos más podemos ingresar.
+
+Crea un menú con opciones que serán seleccionadas por el usuario usando un prompt, las salidas de las operaciones seleccionadas por el usuario se pueden mostrar en pantalla y  por consola.
+*/
+/*
+
+const agenda {
+tamaño
+const contactos = [] esto seria un array
+
+ahora van los metodos:
+
+switch{
+aniadirContacto(Contacto){
+contactos.push
+}
+existeContacto(Contacto){
+if (el contacto que ongresa ya existe){
+documente= ya existe} else{ no exite}
+}
+listarContactos{
+buscar algun metodo de array que liste todos los contactos
+}
+buscarContacto{
+metodo find de array para enconrtrar los contactos
+}
+eliminarContacto(Contacto c){
+el metodo split de array 
+}
+agendaLlena{
+if(agenda.tamaño=10 o numero que ingresa la persona){
+documet(agenda llena)}else{document(agenda no llena)}
+}
+huecosLibres{
+while(tamaño.agenda<=10 o tamaño elegido){
+let huecos ocupados= 0
+huecos ocupados ++
+}
+break
+huecosLibres=10-huevos ocupados
+}
+
+prompt pidiendo el tamaño de la agenda
+un bucle for para ir agregando contactos
+switch{
+aca en el switch ponemos todas las opciones y llamamos a la funcion para que se ejecuten
+1aniadirContacto(Contacto): Añade un contacto a la agenda, sino la agenda no puede almacenar más contactos indicar por pantalla.
+2existeContacto(Contacto): indica si el contacto pasado existe o no.
+3listarContactos(): Lista toda la agenda
+4buscarContacto(nombre): busca un contacto por su nombre y muestra su teléfono.
+5eliminarContacto(Contacto c): elimina el contacto de la agenda, indica si se ha eliminado o no por pantalla
+6agendaLlena(): indica si la agenda está llena.
+7huecosLibres(): indica cuántos contactos más podemos ingresar.
+default
+}confirm(prompt(desea realizar otra operacion))
+
+*/
