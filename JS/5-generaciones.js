06c40ae7e53b09174490f8ac58e96569be9e8d9a
LourdesFrrl <ferraiolimarialourdesfacultad@gmail.com>
2025-05-06 19:21:40 -0300

    complete el ejercicio 5

--- JS/5-generaciones.js
+++ JS/5-generaciones.js
@@ -44,3 +44,73 @@ math.randon.mathfloor (o algo asi)
 
 }
 */
+
+class Persona {
+    constructor(nombre, edad, dni, sexo, peso, altura, añoNacimiento) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+        this.sexo = sexo;
+        this.peso = peso;
+        this.altura = altura;
+        this.añoNacimiento = añoNacimiento;
+    }
+
+    mostrarGeneracion() {
+        let generacion = "";
+        let rasgo = "";
+
+        if (this.añoNacimiento >= 1994 && this.añoNacimiento <= 2010) {
+            generacion = "Generación Z";
+            rasgo = "Irreverencia";
+        } else if (this.añoNacimiento >= 1981 && this.añoNacimiento <= 1993) {
+            generacion = "Generación Y (Millennials)";
+            rasgo = "Frustración";
+        } else if (this.añoNacimiento >= 1969 && this.añoNacimiento <= 1980) {
+            generacion = "Generación X";
+            rasgo = "Obsesión por el éxito";
+        } else if (this.añoNacimiento >= 1949 && this.añoNacimiento <= 1968) {
+            generacion = "Baby Boom";
+            rasgo = "Ambición";
+        } else if (this.añoNacimiento >= 1930 && this.añoNacimiento <= 1948) {
+            generacion = "Silent Generation";
+            rasgo = "Austeridad";
+        } else {
+            generacion = "Generación desconocida";
+            rasgo = "No identificado";
+        }
+
+        document.writeln(`${this.nombre} pertenece a la ${generacion} y su rasgo característico es: ${rasgo}.<br>`);
+    }
+
+    esMayorDeEdad() {
+        if (this.edad >= 18) {
+            document.writeln(`${this.nombre} es mayor de edad.<br>`);
+        } else {
+            document.writeln(`${this.nombre} es menor de edad.<br>`);
+        }
+    }
+
+    mostrarDatos() {
+        document.writeln(
+            `Nombre: ${this.nombre}<br>
+            Edad: ${this.edad}<br>
+            DNI: ${this.dni}<br>
+            Sexo: ${this.sexo}<br>
+            Peso: ${this.peso} kg<br>
+            Altura: ${this.altura} m<br>
+            Año de nacimiento: ${this.añoNacimiento}<br><br>`
+        );
+    }
+
+    generaDNI() {
+        this.dni = Math.floor(10000000 + Math.random() * 90000000);
+        document.writeln(`DNI generado: ${this.dni}<br>`);
+    }
+}
+
+const persona1 = new Persona("Lourdes", 27, null, "F", 60, 1.55, 1998);
+persona1.generaDNI();        
+persona1.mostrarDatos();     
+persona1.mostrarGeneracion();
+persona1.esMayorDeEdad();    
